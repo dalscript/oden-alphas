8f2dde201bb2e16abc5ee83e33aaff9799944930
Dallin Osmun <dosmun@gmail.com>
2022-04-25 15:39:56 -0600

    initial commit

---
+++ install.ps1
@@ -0,0 +1,60 @@
+#!/usr/bin/env pwsh
+# Copyright 2018 the Deno authors. All rights reserved. MIT license.
+# TODO(everyone): Keep this script simple and easily auditable.
+
+$ErrorActionPreference = 'Stop'
+
+if ($v) {
+  $Version = "v${v}"
+}
+if ($args.Length -eq 1) {
+  $Version = $args.Get(0)
+}
+
+$OdenInstall = $env:ODEN_INSTALL
+$BinDir = if ($OdenInstall) {
+  "$OdenInstall\bin"
+} else {
+  "$Home\.oden\bin"
+}
+
+$OdenZip = "$BinDir\oden.zip"
+$OdenExe = "$BinDir\oden.exe"
+$Target = 'x86_64-pc-windows-msvc'
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$OdenUri = if (!$Version) {
+  "https://github.com/dalscript/oden/releases/latest/download/oden-${Target}.zip"
+} else {
+  "https://github.com/dalscript/oden/releases/download/${Version}/oden-${Target}.zip"
+}
+
+if (!(Test-Path $BinDir)) {
+  New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+Invoke-WebRequest $OdenUri -OutFile $OdenZip -UseBasicParsing
+
+if (Get-Command Expand-Archive -ErrorAction SilentlyContinue) {
+  Expand-Archive $OdenZip -Destination $BinDir -Force
+} else {
+  if (Test-Path $OdenExe) {
+    Remove-Item $OdenExe
+  }
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  [IO.Compression.ZipFile]::ExtractToDirectory($OdenZip, $BinDir)
+}
+
+Remove-Item $OdenZip
+
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
+  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+  $Env:Path += ";$BinDir"
+}
+
+Write-Output "Oden was installed successfully to $OdenExe"
+Write-Output "Run 'oden --help' to get started"
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# Copyright 2019 the Deno authors. All rights reserved. MIT license.
+# TODO(everyone): Keep this script simple and easily auditable.
+
+set -e
+
+if ! command -v unzip >/dev/null; then
+	echo "Error: unzip is required to install Oden (see: https://github.com/dalscript/oden_install#unzip-is-required)." 1>&2
+	exit 1
+fi
+
+if [ "$OS" = "Windows_NT" ]; then
+	target="x86_64-pc-windows-msvc"
+else
+	case $(uname -sm) in
+	"Darwin x86_64") target="x86_64-apple-darwin" ;;
+	"Darwin arm64") target="aarch64-apple-darwin" ;;
+	*) target="x86_64-unknown-linux-gnu" ;;
+	esac
+fi
+
+if [ $# -eq 0 ]; then
+	oden_uri="https://github.com/dalscript/oden/releases/latest/download/oden-${target}.zip"
+else
+	oden_uri="https://github.com/dalscript/oden/releases/download/${1}/oden-${target}.zip"
+fi
+
+oden_install="${ODEN_INSTALL:-$HOME/.oden}"
+bin_dir="$oden_install/bin"
+exe="$bin_dir/oden"
+
+if [ ! -d "$bin_dir" ]; then
+	mkdir -p "$bin_dir"
+fi
+
+curl --fail --location --progress-bar --output "$exe.zip" "$oden_uri"
+unzip -d "$bin_dir" -o "$exe.zip"
+chmod +x "$exe"
+rm "$exe.zip"
+
+echo "Oden was installed successfully to $exe"
+if command -v oden >/dev/null; then
+	echo "Run 'oden --help' to get started"
+else
+	case $SHELL in
+	/bin/zsh) shell_profile=".zshrc" ;;
+	*) shell_profile=".bash_profile" ;;
+	esac
+	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+	echo "  export ODEN_INSTALL=\"$oden_install\""
+	echo "  export PATH=\"\$ODEN_INSTALL/bin:\$PATH\""
+	echo "Run '$exe --help' to get started"
+fi
